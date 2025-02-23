bea56512d6e4fc89c24e3612aad846f74d7c26dc
CharlieWuRichi <charliewu@richitech.com.tw>
2025-02-23 18:54:39 +0800

    修復 PWA manifest 404 問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
     { path: '/', component: () => import('../views/Home.vue') },
@@ -6,7 +6,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(), // ✅ 改成 Hash 模式，避免 GitHub Pages 404！
     routes,
 });
 

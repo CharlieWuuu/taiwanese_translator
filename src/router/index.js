1a98e2981e2bbc722fcb52eb9d9a5030492e3e9b
CharlieWuRichi <charliewu@richitech.com.tw>
2025-02-23 19:12:57 +0800

    修復 PWA manifest 404 問題

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
     history: createWebHistory('/taiwanese_translator/'), // ✅ 直接寫死 GitHub Pages 的 base
     routes: [
-        { path: '/', component: () => import('../views/Home.vue') },
+        { path: '/home', component: () => import('../views/Home.vue') },
         { path: '/about', component: () => import('../views/About.vue') },
     ],
 });
